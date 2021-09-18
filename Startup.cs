de201ec794e68249c8d444f8b11f605e381cba87
Folkens <bf853817@ohio.edu>
2021-09-18 19:32:07 -0400

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mis4200_sd2.Startup))]
+namespace mis4200_sd2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
